99b14972a625bdddeed6ee595bcc8886b31fc785
Caleb <calebbradley30@yahoo.com>
2013-09-05 22:43:11 -0400

    model wrk

---
+++ expandContract.js
@@ -0,0 +1,41 @@
+function createModal (){
+	var Modal=$('div class="Modal"></div>')
+	$('body').append()
+}
+
+
+function toggleModal (){
+	$('.modal').toggleClass('.active')
+}
+
+
+
+
+
+function expand(element){
+	var target = document.getElementById(element);
+	var h = target.offsetHeight;
+	var sh = target.scrollHeight;
+	var loopTimer = setTimeout('expand(\''+element+'\')',8);
+	if(h < sh){
+		h += 5;
+	} else {
+		clearTimeout(loopTimer);
+	}
+	target.style.height = h+"px";
+}
+function retract(element){
+	var target = document.getElementById(element);
+	var h = target.offsetHeight;
+	var loopTimer = setTimeout('retract(\''+element+'\')',8);
+	if(h > 0){
+		h -= 5;
+	} else {
+		target.style.height = "0px";
+		clearTimeout(loopTimer);
+	}
+	target.style.height = h+"px";
+}
+
+
+
